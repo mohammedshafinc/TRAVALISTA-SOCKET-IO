01a81e44d77586c5af1e91cc8663aa2e7be12c1c
mohammed shafin <shafinms21@gmail.com>
2024-04-05 14:11:44 +0530

    socket server functuionaity creted

--- app.js
+++ app.js
@@ -3,21 +3,63 @@ require("dotenv").config();
 const cors = require("cors");
 
 const mongoose = require("mongoose");
-const port = process.env.PORT 
-const DB_url = process.env.MONGO_URI
+const port = process.env.PORT;
+const DB_url = process.env.MONGO_URI;
 
-
-const express = require('express')
-const app = express()
-const httpServer = require('http').createServer(app)
-const io = require('socket.io')(httpServer, {cors:{origin:'*'}
-})
+const express = require("express");
+const app = express();
+const httpServer = require("http").createServer(app);
+const io = require("socket.io")(httpServer, { cors: { origin: "*" } });
 
 const users = [];
-app.use(cors())
+app.use(cors());
 
-mongoose.connect(DB_url).then(()=>{
-    console.log('connected to mongodb');
-}).catch((err)=>{console.error('failed to connect mongo db' ,err);})
+mongoose
+    .connect(DB_url)
+    .then(() => {
+        console.log("connected to mongodb");
+    })
+    .catch((err) => {
+        console.error("failed to connect mongo db", err);
+    });
 
+const { postchats } = require("./controllers/chatcontroller");
 
+io.use(async (socket, next) => {
+    socket.userid = socket.handshake.auth.userid;
+    next();
+}).on("connection", (sokcet) => {
+    console.log("a user connected");
+    socketId = socket.id;
+    const user = { socketid: socketId, userid: socket.userid };
+    users.push(user);
+
+    socket.on("sendMessage", (data) => {
+        const { message, reciever, sender } = data;
+        postchats(message, reciever, sender);
+        data.date = new Date();
+
+        const recipient = users.find((user) => user.userid === reciever);
+
+        if (recipient) {
+            io.to(recipient.socketid).emit("recievedMessage", data);
+        } else {
+            console.log("no reciever found");
+        }
+    });
+
+    socket.on("disconnet", () => {
+        console.log(" a user disconnected");
+
+        let index = users.findIndex((e) => e.sockeid === socket.id);
+
+        if (index !== -1) {
+            users.splice(index, 1);
+        } else {
+            console.log("users not found");
+        }
+        console.log(users, "disconnection");
+    });
+});
+
+httpServer.listen(port, () => console.log(`listening on ${port}`))
--- controllers/chatcontroller.js
+++ controllers/chatcontroller.js
@@ -1,1 +1,12 @@
 const chats = require('../models/chatschema')
+
+module.exports = {
+    postchats : async (message,reciever, sender) => {
+        const chat = new chats({
+            message,
+            reciever,
+            sender
+        });
+        await chat.save()
+    }
+}
